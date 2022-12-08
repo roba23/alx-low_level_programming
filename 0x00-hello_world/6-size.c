7e97f0103eaad103e4854f3bb80c0bde378d828c
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-08 15:42:29 -0800

    great

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,14 @@
  */
 int main(void)
 {
-	printf("Size of a char%lu:", Sizeof(char));
-	printf("Size of an int%lu:", Sizeof(int));
-	printf("Size of a long int%lu:", Sizeof(long int));
-	printf("Size of a long long int%lu:", Sizeof(long long int));
-	printf("Size of a float%lu:", Sizeof(float));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("Size of a char%lu:", (unsigned long)sizeof(a));
+	printf("Size of an int%lu:", (unsigned long)sizeof(b));
+	printf("Size of a long int%lu:", (unsigned long)sizeof(c));
+	printf("Size of a long long int%lu:", (unsigned long)sizeof(d));
+	printf("Size of a float%lu:", Sizeof(e));
 }
