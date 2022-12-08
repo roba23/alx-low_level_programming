aa127f8b4b0a56c1c63eb8260cea90784318a322
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-08 15:32:42 -0800

    great

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main-this is description of main
+ * Return:returns o if sucessful
+ *
+ */
+int main(void)
+{
+	printf("Size of a char%lu:", Sizeof(char));
+	printf("Size of an int%lu:", Sizeof(int));
+	printf("Size of a long int%lu:", Sizeof(long int));
+	printf("Size of a long long int%lu:", Sizeof(long long int));
+	printf("Size of a float%lu:", Sizeof(float));
+}
