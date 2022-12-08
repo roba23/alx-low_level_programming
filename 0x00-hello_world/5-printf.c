08433e34908e9cb2dd876328fc430c7e4de10217
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-08 15:21:13 -0800

    ohh

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-the main function
+ *
+ * Return:retun o if it is sucessfull
+ */
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
