85be9a4bf136edae7a36020328085ad24203ecc4
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-08 17:38:08 -0800

    ok

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-descript
+ *
+ * Return-returns
+ */
+ extern FILE *stderr;
+
+int main(void)
+{ 
+	fprintf(stderr,"%s %c %s %s", "and that piece of art is useful\" ", '-' ,"Dora korpar,", "2015-10-19");
+        return (0);
+}
