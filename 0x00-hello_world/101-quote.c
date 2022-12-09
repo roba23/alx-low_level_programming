9efb6c1690a765efec82d4acfe905638a780d362
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-09 00:02:27 -0800

    mas

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 	fprintf(stderr, "%s", "and that piece of art is useful");
-	fprintf(stderr, "%s", "\" - Dora korpar, 2015-10-19\n");
+	fprintf(stderr, "%s", "\" - Dora korpar, 2015-10-19");
 	return (1);
 }
