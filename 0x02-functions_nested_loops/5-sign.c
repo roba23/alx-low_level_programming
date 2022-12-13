5b8053efeca75c68d3f093a57bd0d8e011116664
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-13 11:57:14 -0800

    identify sign for numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_sign - determines the sign of an integer
+ * @n: any integer accepted from the user or other program
+ *
+ * Return: return 1 if n is greater than zero
+ * 0 if n is zero
+ * -1 if the number is lessthan zero
+ */
+int print_sign(int n)
+{
+	int sgn;
+
+	if (n < 0)
+	{
+		sgn = -1;
+		_putchar('-');
+		_putchar('1');
+	}
+	else if (n > 0)
+	{
+		sgn = 1;
+		_putchar('+');
+	}
+	else
+	{
+		sgn = 0;
+		_putchar('0');
+	}
+	return (sgn);
+}
