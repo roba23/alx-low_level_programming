97eec12e529c7b8793e23edd557bd4f171421bf6
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-13 11:18:10 -0800

    create a script to differentiate numbers from other characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * _isalpha - checks if a character is a
+ * number or not
+ * @c:integer value accepted
+ *
+ * Return: return 0 if it is a number
+ * 1,if it is not a number
+ */
+int _isalpha(int c)
+{
+	int d = isalpha(c);
+	int m;
+	if (d == 0)
+		m = 0;
+	else
+		m = 1;
+	return (m);
+}
+
