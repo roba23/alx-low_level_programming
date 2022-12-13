d9c7f9fe97629dab938934ef7bdcea17dbaf6305
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-13 12:25:18 -0800

    find absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - find the absolute value of a number
+ *
+ * Return: returns the absolute value of a number
+ */
+int _abs(int a)
+{
+	int m = abs(a);
+	return (m);
+
+}
