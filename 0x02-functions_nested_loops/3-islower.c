c808407880282ec57e27cf346006659b8d3d1af4
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-13 08:51:20 -0800

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - determines wether a number
+ * is lower case or not
+ * @c: integer
+ *
+ * Return: return 1 if c is lowercase
+ * return 0 otherwise
+ */
+int _islower(int c)
+{
+	int ret;
+
+	ret = islower(c);
+	return (ret);
+}
+
+
+
