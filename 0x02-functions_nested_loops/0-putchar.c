2c40c1aab4b7003c9364978c191134718f5f5047
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-13 06:02:13 -0800

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,12 +8,11 @@
  */
 int main(void)
 {
-	char m[9] = "_putchar";
+	char m[9] = "_putchar\n";
 	int i;
 	int length = sizeof(m) / sizeof(m[0]);
 
 	for (i = 0; i < length; i++)
 		_putchar(m[i]);
-	_putchar('\n');
 	return (0);
 }
