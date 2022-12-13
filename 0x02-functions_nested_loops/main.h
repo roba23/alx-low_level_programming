d64fa3d7461c5b91f5299a2741d4e7d9b6ddffa7
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-13 05:19:58 -0800

    add main.h file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+int _putchar(char c);
+void print_alphabet_x10(void);
+
+void print_alphabet_x10(void)
+{
+	char r;
+
+	for ( r='a'; r <= 'z'; r++)
+		_putchar(r);
+	_putchar('\n');
+	return (0);
+}
