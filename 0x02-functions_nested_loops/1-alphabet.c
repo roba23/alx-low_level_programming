78bf617d3390d81f1897271552bc48540e934fb3
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-13 06:28:43 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,11 @@
  * Return: 0 if succesful.
  * error, print 1
  */
-int main(void)
+void print_alphabet(void)
 {
 	char d;
 
 	for (d = 'a'; d < 'z'; d++)
 		_putchar(d);
 	_putchar('\n');
-	return (0);
 }
