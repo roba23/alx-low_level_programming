163212e6733579f6f9394272f98c6c150c19a7a3
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-13 14:37:00 -0800

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - displays the multiplication table
+ *
+ * Return: returns nothing
+ */
+void times_table(void)
+{
+	int a, b, mul;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			mul = a * b;	
+			printf("%d, ", mul);
+
+		}
+		printf("\n");
+	}
+}
