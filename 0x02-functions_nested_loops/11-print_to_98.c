0fc15ce1c690250440afa6b027ff081ebf031b34
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-13 16:24:56 -0800

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,25 +12,17 @@ void print_to_98(int n)
 
 	if (n < 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 		{
-			if (i == 98)
-				printf("%d", i);
-			else
-			        printf("%d, ", i);
+			printf("%d, ", i);
 		}
 	}
 	else if (n > 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 		{
-			if (i == 98)
-				printf("%d", i);
 			printf("%d, ", i);
 		}
 	}
-	else
-	{
-		printf("98");
-	}
+	
 }
