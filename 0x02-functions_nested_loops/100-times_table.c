3950abcfe63ebbb234b22181d844721c1685201c
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-13 23:32:38 -0800

    time table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_times_table - print times table
+ * @n:argument
+ *
+ * Return: returns nothing
+ */
+void print_times_table(int n)
+{
+	int i,j,mul;
+
+	if (n < 15 && n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				mul = i * j;
+				if (j == n)
+					printf("%4d", mul);
+				else
+					printf("%4d,", mul);
+
+
+
+
+			}
+			printf("\n");
+
+		}
+	}
+}
