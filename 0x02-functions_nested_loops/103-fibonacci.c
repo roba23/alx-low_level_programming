7a529db5fcc967d92a024646ca3a08e850064c70
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-14 04:51:40 -0800

    add fibonachi series

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of even valued terms
+ * in fibonacci series not excedding 4,000,000
+ * Return: return 0 if succesfull
+ */
+int main(void)
+{
+	long int i, j, sum, d, temp;
+
+	i = 0;
+	j = 0;
+	while (sum < 4000000)
+	{
+		temp = i + j;
+		if (temp % 2 == 0)
+			sum += temp;
+		i = j;
+		j = sum;
+	}
+	printf("%ld\n", sum);
+	return (0);
+
+
+}
