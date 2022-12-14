fb44a534ed39eead7f22596825db6ac06d333db9
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-14 05:31:27 -0800

    print fibonacci series up to the 98th element

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints finonacci series
+ * up to the 98th element
+ * Return: return 0 on sucess
+ * on error return 1
+ */
+int main(void)
+{
+	int i, j, sum, d;
+
+	i = 0;
+	j = 1;
+	sum = 0;
+	for (d = 0; d < 98; d++)
+	{
+		sum = i + j;
+		printf("%d, ", sum);
+		i = j;
+		j = sum;
+	}
+	printf("\n");
+	return (0);
+}
+
