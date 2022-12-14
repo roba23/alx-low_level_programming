3bf3103be6d32c00a029557a5e2aa8bbe6578311
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-14 03:04:58 -0800

    fibonacci series

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,11 +14,11 @@ int main(void)
 	{
 		sum = i + j;
 		if (d == 0)
-			printf("%1ld,", sum);
+			printf("%3ld,", sum);
 		else if (d < 49) 
-			printf("%1ld,", sum);
+			printf("%3ld,", sum);
 		else if (d == 49)
-			printf("%1ld", sum);
+			printf("%3ld", sum);
 		i = j;
 		j = sum;
 	}
