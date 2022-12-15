27330e2cb25621f05abeb8316913ba4939ebc015
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-15 05:25:25 -0800

    git debug task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,15 +10,15 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
-/**
+/*
+ * printf- this is a comment i dont
+ * @\\o/\n:control m and m
  *	while (i < 10)
  *	{
  *		putchar(i);
  *	}
  */
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
