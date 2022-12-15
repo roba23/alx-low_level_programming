7d9720b1e77f2033e9f556d197aebd55f1dd6b1d
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-15 00:30:25 -0800

    debugging task 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - function tests the 
+ * positive or negative for zero
+ * Return: return 0 if sucessfull
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	positive_or_negative(n);
+	return (0);
+}
