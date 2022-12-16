8be3fc67f4f4f0f571fef8e37dc6a305c87ba339
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-16 00:59:23 -0800

    task 1 in more functions

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+int _isdigit(int c)
+
+/**	
+ * _isdigit - determines if an input is digit or not
+ * @c: input in number format
+ * Return: 1 if it is digit
+ * 0,other wise
+ */
+int _isdigit(int c)
+{
+	int m;
+
+	m = isdigit(c); 
+	if (c != 0)
+		return (1);
+	else 
+		return (0);
+}
+
+
