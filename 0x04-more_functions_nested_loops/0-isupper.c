5d143a1646b7e5e163ec041215dd9eeb27b761f3
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-16 11:27:23 -0800

    upper or lower

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,24 +1,14 @@
-#include <ctype.h>
-#include "main.h" 
+#include "main.h"
 
 /**
- * _isupper - determines if a character is upper or
- * or lower case
- * @c: input in number formar
- * Return: return 1 if upper case 
- *  for any other case
+ * _isupper - a function that checks for uppercase character
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-	int m;
-
-	m = isupper(c);
-	if (m != 0)
-	{
-		return (1);
-	}
+	if ((c >= 'A') && (c <= 'Z'))
+	return (1);
 	else
-	{
-		return (0);
-	}
+	return (0);
 }
