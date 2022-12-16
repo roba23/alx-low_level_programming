3023e6d3b2a5c107e48f658aff90b6417445ff55
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-16 06:31:29 -0800

    trying to fix header file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _isupper(int c);
 int _putchar(char c);
 int _isdigit(int c);
@@ -5,3 +8,4 @@ int mul(int a, int b);
 void print_numbers(void);
 void more_numbers(void);
 void print_long(int value);
+#endif
