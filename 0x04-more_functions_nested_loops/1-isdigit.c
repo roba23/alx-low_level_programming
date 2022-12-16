7249708f47f2df3f4014db86cb99d2f89cde1d2b
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-16 05:31:47 -0800

    the checker is working again

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include <ctype.h>
 #include "main.h"
 
-/**	
+/**
  * _isdigit - determines if an input is digit or not
  * @c: input in number format
  * Return: 1 if it is digit
@@ -11,10 +11,10 @@ int _isdigit(int c)
 {
 	int m;
 
-	m = isdigit(c); 
+	m = isdigit(c);
 	if (m != 0)
 		return (1);
-	else 
+	else
 		return (0);
 }
 
