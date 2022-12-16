9870933f060f3450286811b0e499228931fb0271
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-15 23:39:49 -0800

    script to determine if upper or lower case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper: determines wether a character is
+ * uper or lower case
+ * @c: input in number format
+ * Return: 1 if uppercase
+ * 0 otherwise
+ */
+int _isupper(int c)
+{
+	if(isupper(c) != 0)
+		return (1);
+	else
+		return (0);
+}
