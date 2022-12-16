c328b9d2267617e9fe1f07f630ef7c6d33c6427a
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-16 11:06:40 -0800

    fizz_buzz fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,12 +13,12 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 5 == 0 && i % 3 == 0)
+			printf("%s ", "FizzBuzz");
+		else if (i % 3 == 0)
 			printf("%s ", "Fizz");
 		else if (i % 5 == 0)
 			printf("%s ", "Buzz");
-		else if (i % 5 == 0 && i % 3 == 0)
-			printf("%s ", "FizzBuzz");
 		else
 			printf("%d ", i);
 	}
