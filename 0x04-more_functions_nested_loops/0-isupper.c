83854a5a350500b49f0c52e4d5bc6b64365c725e
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-18 00:55:57 -0800

    determine if a character is upper ofr lowe case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <ctype.h>
 
 /**
  * _isupper - a function that checks for uppercase character
@@ -7,7 +8,7 @@
  */
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
+	if (isupper(c))
 		return (1);
 	else
 		return (0);
