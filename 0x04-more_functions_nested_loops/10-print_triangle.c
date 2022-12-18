5a3739ae4694e2b3c641a984f3ff9cda590f578c
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-18 05:48:09 -0800

    patterns are a great way to understand loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,6 +9,12 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
 	for (i = 1; i <= size; i++)
 	{
 		for (j = i; j < size; j++)
@@ -23,5 +29,6 @@ void print_triangle(int size)
 		
 	}
 	_putchar('\n');
+	}
 }
 
