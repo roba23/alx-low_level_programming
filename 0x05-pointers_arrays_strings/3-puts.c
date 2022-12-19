2beb06d57020fd92d93b681591c8b32c9cda3711
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-19 12:30:07 -0800

    function to print out string using puts() function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts -print a string passed to it
+ * @str: string to print to stdout
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
