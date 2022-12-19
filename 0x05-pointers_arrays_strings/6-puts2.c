855621f9fdadeec6d9d7cdd4f6327f959edbfebe
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-19 15:06:39 -0800

    print every other string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reverses a string.
+ * @s: input string to reverse.
+ */
+void puts2(char *str)
+{
+	int i, size;
+
+	i = 0;
+	size = strlen(str) - 1;
+        while(i < size)
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
