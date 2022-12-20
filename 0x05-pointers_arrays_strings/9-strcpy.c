21e6ed298a8e86d3107eff26178538f56edd8bbd
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-20 04:05:23 -0800

    copy string from one variable to another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - copy string 
+ * @dest: ok
+ * @src: ok
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
