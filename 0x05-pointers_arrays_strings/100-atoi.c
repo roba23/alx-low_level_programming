45a98734d200993c4a9df103513c0981af436278
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-22 03:47:03 -0800

    fixed it

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,25 +11,31 @@ int _atoi(char *s)
 	int sign = 1;
 	int n = 0;
 	int flag;
+	int oc;
 
 	i = j = 0;
 	size = strlen(s);
 	flag = 0;
+	oc = 0;
 	for (i = 0; i < size; i++)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			flag = 1;
 			n *= 10;
-			n  += (s[i]-'0');
+			n  += (s[i] - '0');
 		}
-	
-		else if (s[i] == '-' && flag ==0)
+		else if (s[i] != ' ' && s[i] != '-' && s[i] != '+' && flag == 0)
 		{
-			sign = -1;
-			
+			oc = 1;
+			sign = 1;
 		}
-		else if (s[i] == ' ' && flag == 1)
+		if ((s[i] == '-' || s[i] == '+') && oc == 0)
+		{
+			if (s[i] == '-')
+				sign *= -1;
+		}
+		if (s[i] == ' ' && flag == 1)
 		{
 			break;
 		}
@@ -38,5 +44,5 @@ int _atoi(char *s)
 	n *= sign;
 	return (n);
 
-	
+
 }
