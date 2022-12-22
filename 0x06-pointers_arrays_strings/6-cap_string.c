8a6656a63c8f79fbf9a21649a6b72a75dbab374b
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-22 13:43:49 -0800

    array

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+/**
+ * cap_string - convert lower case characters to uppercase
+ * @s: argument
+ * Return: char
+ */
+char *cap_string(char *s)
+{
+		int i, j;
+		char sep[] = " \t\n,;.!?\"(){}";
+
+		i = 1;
+		if (s[0] >= 'a' && s[0] <= 'z')
+			s[0] -= 32;
+		while (s[i] != '\0')
+		{
+		for (j = 0; sep[j] != '\0'; j++)
+			if (s[i - 1] == sep[j] && (s[i] >= 'a' && s[i] <= 'z'))
+				s[i] -= 32;
+		i++;
+		}
+		return (s);
+}
