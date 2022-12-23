7583cf53b96782162ce8b5d39ca31d91d6c3999c
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-23 05:20:31 -0800

    replace characters with numbers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * leet - replace character in strings with numbers
+ * @a: character
+ * Return: character with i e and a replaced by numbers
+ */
+char *leet(char *a)
+{
+	char c[10] = "aAeEoOtTlL";
+	char r[5] = "43071";
+        long int i, j, size;
+
+	size = strlen(a);
+	printf("size:%ld\n", size);
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (a[i] == c[j])
+				a[i] = r[j / 2];
+
+		}
+	}
+	return (a);
+
+}
