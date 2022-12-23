8176fedca58877c6f53156a33439f586bef3c7ad
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-23 03:23:58 -0800

    reverse an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_arry - function to reverse an array
+ * @a: array to be reversed
+ * @n: the size of tghe array
+ * Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp;
+
+	for (i = 0; i <= n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
