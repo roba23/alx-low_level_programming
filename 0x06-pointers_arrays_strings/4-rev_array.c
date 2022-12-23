1d765ea4024fcf8c33b0c08d2b70a3e26a829a17
Robel Gebremariam <dekeborobel@gmail.com>
2022-12-23 03:39:23 -0800

    reverse an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,8 +11,7 @@ void reverse_array(int *a, int n)
 	int temp;
 	int size;
 
-	size = sizeof(a) / sizeof(int *);
-	for (i = 0; i <= size / 2; i++)
+	for (i = 0; i <= n / 2; i++)
 	{
 		temp = a[i];
 		a[i] = a[n - 1 - i];
