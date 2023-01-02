c565a1ec8962bf33c4ae86046ca2620aa1654252
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-02 14:03:18 -0800

    task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,12 +8,11 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	i = j = 0;
 	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j < 7; j++)
+		for (j = 0; *(a[i] + j) != '\0'; j++)
 		{
-			_putchar(a[i][j]);
+			_putchar(*(a[i] + j));
 
 		}
 		_putchar('\n');
