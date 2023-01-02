43c217170df4a914aafde7362a555405c3905501
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-02 06:08:45 -0800

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+/**
+ * _strchr - locate a character in a string
+ * @s: the string to be checked
+ * @c: character to be located
+ * Return: return the adress of the first character occurence
+ */
+char *_strchr(char *s, char c)
+{
+	char *p;
+	int i;
+
+	p = '\0';
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			p = &s[i];
+			break;
+		}
+		++i;
+	}
+	return (p);
+}
