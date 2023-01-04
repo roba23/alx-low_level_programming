48c7fb6491d23978644dbd72c353ce644c856a52
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-04 12:34:11 -0800

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,9 @@
 #include "main.h"
 /**
- * _sqrt_recursion - find the square root of a number
- * @n: the number
- * Return: return the root of a number
- * -1 if it does not have any root
- */
-int find_root(int i, int n);
-int _sqrt_recursion(int n)
-{
-	return (find_root(1, n));
-}
-/**
  * find_root - determine wether the number has root or not
  * @i: the number to start checking from usually 1
  * @n: the number
+ * Return: return square root
  */
 int find_root(int i, int n)
 {
@@ -23,3 +13,16 @@ int find_root(int i, int n)
 		return (-1);
 	return (find_root(i + 1, n));
 }
+
+
+/**
+ * _sqrt_recursion - find the square root of a number
+ * @n: the number
+ * Return: return the root of a number
+ * -1 if it does not have any root
+ */
+int _sqrt_recursion(int n)
+{
+	return (find_root(1, n));
+}
+
