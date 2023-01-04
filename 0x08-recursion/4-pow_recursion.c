b978002ece8f701e49d3cb202492bd3e6821d657
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-04 11:33:24 -0800

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _pow_recursion - calculate number x to the power of y
+ * @x:base  
+ * @y:power
+ * Return: return result
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else 
+		return (x * _pow_recursion(x, y - 1));
+}
