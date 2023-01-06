cced12de9dc182265aca48ae6dfd8ad196968392
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-06 13:52:52 -0800

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the name of program
+ * @argc: count arguments passed to a main function
+ * @argv: an array containing values of the arguments
+ * Return: return 0 if succesfull
+ */
+int main(int argc, char *argv[]) 
+{
+	if (argc >= 1)
+		printf("%s\n", argv[0]);
+	return (0);
+}
