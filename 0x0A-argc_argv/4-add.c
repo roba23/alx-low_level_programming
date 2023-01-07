40d8d95b78987e82d4d9c2296862f9f877b84897
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-07 12:39:42 -0800

    fixed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,6 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 /**
+ * check - check if an argument pased from command line is digit or not
+ * @a: string
+ * Return: return 0 if it is digit and 1 if it is not
+ */
+int check(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (isdigit(a[i]) == 0)
+			return (1);
+	}
+	return (0);
+}
+/**
  *main - adds all arguments together if they are digits.
  *@argc: argument count only accepts ints separated by spaces.
  *@argv: argument vector
@@ -12,22 +28,16 @@ int main(int argc, char *argv[])
 {
 	int sum, i;
 
-	if (argc < 2)
-	{
-		printf("0\n");
-		return (0);
-	}
+	sum = 0;
 	for (i = 1; i < argc; i++)
 	{
-		if (*argv[i] == '0')
-			sum += 0;
-		if (atoi(argv[i]) && *argv[i] != '0')
-			sum += atoi(argv[i]);
-		else
+		if (check(argv[i]))
 		{
 			printf("Error\n");
 			return (1);
 		}
+		else
+			sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
