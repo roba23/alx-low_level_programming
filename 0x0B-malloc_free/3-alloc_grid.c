1499839b59bd945cdb0b76b70bc1cbef57311dd6
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-09 14:24:30 -0800

    task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * alloc_grid - create a meomry space for 2d array
+ * @width: number of columuns
+ * @height: number of rows
+ * Return: return pointer of pointers
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **p;
+
+	if (width < 0 || height < 0)
+		return (NULL);
+	i = j = 0;
+	p = malloc(height * (int)sizeof(int **));
+	for (i = 0; i < height; i++)
+		p[i] = malloc(width * (int)sizeof(int *));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			p[i][j] = 0;
+		}
+	}
+	return (p);
+}
+
+
