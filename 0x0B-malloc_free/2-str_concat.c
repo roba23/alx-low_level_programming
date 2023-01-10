e078fa35d9ea2d1e9f69d4abef8ac3f7a05e2e20
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-10 00:58:46 -0800

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 	p = malloc((sizeof(char) * (s1_size + s2_size)) + 1);
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 		p[i] = s1[i];
 	}
