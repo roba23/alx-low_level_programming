db5296cfef03511426c6245ec7542bdd036293ce
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-10 00:42:10 -0800

    task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * free_grid - free asigned grid
+ * @grid: matrix passed to be filled
+ * @height: the height of grid
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	i = 0;
+	for (i = 0; i < height; i++)
+		free(p[i]);		
+	free(p);
+}
+	
