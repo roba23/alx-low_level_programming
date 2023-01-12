3cf5165f48270e85a0ff315b546451142c018adc
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-12 13:18:41 -0800

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,26 +11,27 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int s1_length, s2_length, sizeBuffer, i, j;
-
+	int s1_length, s2_length, sizeBuffer, i, j;
+	int m;
 	i = j = 0;	
+	m = n;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	s1_length = (unsigned int)strlen(s1);
-	s2_length = (unsigned int)strlen(s2);
-	if (n > s2_length)
-		n = s2_length;
+	s1_length = strlen(s1);
+	s2_length = strlen(s2);
+	if (m >= s2_length)
+		m = s2_length;
 	sizeBuffer = s1_length + s2_length + 1;
 	p = malloc(sizeBuffer * sizeof(char));
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < s1_length; i++)
 		p[i] = s1[i];
-	for (j = 0; j <= n; j++)
+	for (j = 0; j <= m; j++)
 	{
-		if (j == n)
+		if (j == m)
 			p[s1_length + j] = s2[j];
 		else
 			p[j + s1_length] = s2[j];
