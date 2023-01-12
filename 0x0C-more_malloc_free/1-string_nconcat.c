0511a2944ab15d3a65396682f37115321e0a1f81
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-12 08:46:14 -0800

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,6 +35,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		p[i] = s1[i];
 	for (j = 0; j < n; j++)
 		p[j + s1_length] = s2[j];
-	p[s1_length + s2_length + 1] = '\0';
 	return (p);
 }
