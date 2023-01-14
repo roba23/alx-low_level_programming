f554623b3fceecb3561df5007a20cec2f22506d1
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-14 04:15:23 -0800

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,9 +9,6 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *p;
-
-	p  = ptr;
 	if (old_size == new_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
