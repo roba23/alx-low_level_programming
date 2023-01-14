0e8379e58f47465f65b545956eb8a1264058619c
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-13 23:03:40 -0800

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,11 +9,16 @@
 void *_calloc(int nmemb, unsigned int size)
 {
 	char *s;
+	int i;
+
+	i = 0;
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	s = malloc(nmemb * size);
 	if (s == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		s[i] = 0;
 	return (s);
 }
 	
