d82add866317ab058efcf2532b6161e46b29788e
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-16 12:32:08 -0800

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - dog structure with variables name,age and owner
  * @name: name of dog
@@ -8,11 +9,14 @@
  *
  * Description: this struct take attributes of the dog
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
 
