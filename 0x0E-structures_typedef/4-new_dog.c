a10d830e8604252b6c98305aa7dd1b4207081425
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-16 13:57:58 -0800

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - creates new dog
+ * @name: name of dog
+ * @age: age
+ * @owner: owner of dog
+ * Return: dog_t pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *doggy;
+
+	doggy = malloc(sizeof(struct dog));
+	if (doggy == NULL)
+		return (NULL);
+	doggy->name = name;
+	doggy->age = age;
+	doggy->owner = owner;
+	return (doggy);
+}
