c6ae04205905295146841be43818ffa60cda63f9
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-16 12:52:00 -0800

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,10 +13,10 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name);
 	if (d->age == 0)
 		printf("Age: (nil)\n");
-	else 
+	else
 		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
-	else 
+	else
 		printf("Owner: %s\n", d->owner);
 }
