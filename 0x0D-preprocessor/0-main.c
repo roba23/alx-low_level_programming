d6e3bc8ba07b2b8fa4929425fb7636b246972271
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-17 09:52:36 -0800

    task 0

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,16 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return - nothing
+ */
+int main(void)
+{
+	int s;
+
+	s = SIZE + 98;
+	printf("%d\n", s);
+	return (0);
+}
+
