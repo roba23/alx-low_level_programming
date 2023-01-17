79b8d08ba2de6eab4aeb7202ec64e1d52dbc63e0
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-17 01:19:55 -0800

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,20 +15,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = malloc((strlen(name) + 1) * sizeof(char));
-	if (dog->name == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
-	dog->owner = malloc((strlen(owner) + 1) * sizeof(char));
-	if (dog->owner == NULL)
-	{
-		free(dog->owner);
-		free(dog);
-
-		return (NULL);
-	}
 	dog->name = name;
 	dog->age = age;
 	dog->owner = owner;
