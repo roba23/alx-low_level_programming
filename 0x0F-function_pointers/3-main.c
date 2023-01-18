db3d7905ae2ffd1c36043c2b118c9841ae117f66
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-18 14:26:53 -0800

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if (num2 == 0 && (argv[2] == '/' || argv[2] == '%'))
+	if (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%'))
 	{	
 		printf("Error\n");
 		exit(100);
