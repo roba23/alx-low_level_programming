26ef2d5275f5b65bb026d529f90813056fec9598
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-18 15:30:17 -0800

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}};
 	while (i < 5)
 	{
-		if (strcmp(s, ops[i].op) == 0 && !(*(s + 1)))
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
 
 		i++;
