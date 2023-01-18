5ebb00c0f78070b7c7c2faba453b13639e8b1ad2
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-18 13:08:58 -0800

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,12 +11,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || !(array) || !(cmp))
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
-			return i;
+			return (i);
 	}
 	return (-1);
 }
