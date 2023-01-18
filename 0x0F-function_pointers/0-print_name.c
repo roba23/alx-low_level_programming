7ae5785582137ccd2bbf188d8cce00ad5c7f5ceb
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-18 12:22:21 -0800

    fixing task 1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 /**
  * print_name - print name  passed to a function
  * @name: name
- * @f(): the function passed
+ * @f: the function passed
  * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
