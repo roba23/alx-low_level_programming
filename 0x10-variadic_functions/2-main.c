e1e4f8f034270531262c00d26f3c226466218766
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-19 05:40:22 -0800

    task 2

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return:nothing
+ */
+int main(void)
+{
+	print_strings(", ", 2, "Jay", "Django");
+	return (0);
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - print strings given as parameters
+ * @separator: separator used
+ * @n: number of strings to print
+ * Return: nothing
+ */
+void print_strings(char *separator, unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	char *s;
+	char *c = "(nil)";
+
+	va_start(args,n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(args, char *);
+		if(separator != NULL && i > 0)
+		{
+			if (s == NULL)
+				s = c;
+			printf("%s%s", separator, s); 
+		}
+		else
+			printf("%s", s);
+	}
+	printf("\n");
+	va_end(args);
+}
