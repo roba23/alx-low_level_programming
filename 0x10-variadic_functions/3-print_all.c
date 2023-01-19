32c929b2dd8bc8297ced24f8cc82335d32d67e05
Robel Gebremariam <dekeborobel@gmail.com>
2023-01-19 13:46:43 -0800

    broke it down to functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -62,7 +62,7 @@ void print_all(const char * const format, ...)
 	i = 0;
 	len = (int)strlen(format);
 	va_start(args, format);
-	while (i < len && (format != NULL))
+	while (i < len && len > 0)
 	{
 		j = 0;
 		while (j < 4)
