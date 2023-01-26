781636b13bf7432c042c589b8a71e1ef80bd73b6
roba23 <dekeborobel@gmail.com>
2023-01-26 04:59:28 -0800

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_list - function to print all elements of a node in a  linked list
+ * @h: pointer to the first element of the lists
+ * Return: return the length of string store on the node
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 1;
+
+	if (h->str == NULL)
+		printf("[%d] %s\n", 0, "(nil)");
+	else
+		printf("[%d] %s\n", h->len, h->str);
+	if (h->next != NULL)
+	{
+		print_list(h->next);
+		count++;
+	}
+	return (count);
+
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -16,7 +16,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-size_t print_list(const list_t *h)l
+size_t print_list(const list_t *h);
 
 
 
