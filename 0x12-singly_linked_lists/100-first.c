cdfb0574828939d1371426369a17d98017d58fc1
robel gebremariam <dekeborobel@gmail.com>
2023-01-27 12:43:48 +0000

    task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 void __attribute__((constructor)) print();
-void print(void);
+int print(void);
 /**
  * print - print a statment
  * Return:nothing
  */
-void print(void)
+int print(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	return (0);
 }
