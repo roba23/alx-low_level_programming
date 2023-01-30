997aa9007deb1ce1ec7a1ca8ae143444cc1f56ef
robel gebremariam <dekeborobel@gmail.com>
2023-01-30 19:02:35 +0000

    changed folder name

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint - print all elements in a list
+ * @h: adress of the first element
+ * Return: return number of elements 
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+	if (h == NULL)
+		return (0);
+	for (i = 0; h; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}	
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - free list of nodes
+ * @head: pointer to the list passed by value
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	if (*head == NULL)
+		return;
+	*head = (* head)->next;
+	free_listint2(head->);
+	free(head);
+}
