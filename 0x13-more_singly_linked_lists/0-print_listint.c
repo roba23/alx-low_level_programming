3d7360bf963a233d8334c39c265d4efb0f1ded8b
robel gebremariam <dekeborobel@gmail.com>
2023-01-30 19:07:48 +0000

    betty check fixed

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,17 +3,18 @@
 /**
  * print_listint - print all elements in a list
  * @h: adress of the first element
- * Return: return number of elements 
+ * Return: return number of elements
  */
 size_t print_listint(const listint_t *h)
 {
 	size_t i;
+	
 	if (h == NULL)
 		return (0);
 	for (i = 0; h; i++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-	}	
+	}
 	return (i);
 }
