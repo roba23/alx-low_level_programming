e03741e982ad989ca54e37588bb57691b8be4861
robel gebremariam <dekeborobel@gmail.com>
2023-02-02 20:38:07 +0000

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_binary - convert a number to binary and print
+ * @n: number
+ * Return: Nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int result, remainder;
+
+	result = 0.5 * n;
+	remainder = n - (2 * result);
+	n = result;
+	if (n > 0)
+		print_binary(n);
+	_putchar(remainder + '0');
+}
