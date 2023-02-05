2934c976c518305036e155a7d0645a4e51de897f
robel gebremariam <dekeborobel@gmail.com>
2023-02-05 04:07:20 +0000

    task 12

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+#include <stdbool.h>
+/**
+ * find_node - find the node where loop starts
+ * @tortoise: the tortoise
+ * @hare: the hare
+ * Return: the pointer where the loop starts
+ */
+listint_t *find_node(listint_t *tortoise, listint_t *hare)
+{
+	while (1)
+	{
+		if (tortoise == hare)
+			return (tortoise);
+		tortoise = tortoise->next;
+		hare = hare->next;
+	}
+	return (NULL);
+}
+/**
+ * free_listint_safe - free a list if it contains a loop
+ * @head: the pointer to the first node of the singly linked list
+ * Return: return the size of the list freed
+ */
+size_t free_listint_safe(listint_t **head)
+{
+	size_t size;
+	int m;
+	bool met;
+	listint_t *temp, *tortoise, *hare, *ptr, *temp1;
+
+	met = false;
+	temp = tortoise = hare = *head;
+	m = size = 0;
+	if (*head == NULL)
+		return (0);
+	while (temp != NULL)
+	{
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+		if (hare == NULL || tortoise == NULL)
+			break;
+		if (hare == tortoise)
+		{
+			met = true;
+			ptr = find_node(*head, hare);
+			break;
+		}
+		temp = temp->next;
+	}
+	temp = *head;
+	while (temp)
+	{
+		size += sizeof(temp);
+		if (met && (temp->next == ptr))
+			m++;
+		temp1 = temp;
+		temp = temp->next;
+		free(temp1);
+		if (m == 2)
+			break;
+	}
+	*head = NULL;
+	return (size);
+}
