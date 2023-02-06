7f6b35b47464c74ebf2111ca98403e2fb96974fa
robel gebremariam <dekeborobel@gmail.com>
2023-02-06 21:39:05 +0000

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int i;
 	char buffer[65534];
 
-	if (filename == NULL || size == 0)
+	if (filename == NULL || letters == 0)
 		return (0);
 	i = 0;
 	m = letters;
