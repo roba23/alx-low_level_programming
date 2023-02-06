be7f75476dabc37f3727d1c82a05ccf93fe7aa6d
robel gebremariam <dekeborobel@gmail.com>
2023-02-06 21:21:50 +0000

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -32,7 +32,7 @@ ssize_t read_textfile(const char *filename, ssize_t letters)
 	buffer[i] = '\0';
 	lseek(fd, 0, SEEK_SET);
 	i = write(1, buffer, m);
-	if (i < 0)
+	if (i <= 0)
 		return (0);
 	close(fd);
 	return (m);
