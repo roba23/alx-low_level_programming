3adc9b60546662ee43580591678f4b2d7ed0189c
robel gebremariam <dekeborobel@gmail.com>
2023-02-06 21:53:23 +0000

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,16 +21,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
 	r = read(fd, buffer, letters);
-	if (r == -1)
-		return (0);
 	buffer[r] = '\0';
 	lseek(fd, 0, SEEK_SET);
 	w = write(1, buffer, r);
-	if (w == -1 || w != r)
+	if (w == -1 || r == -1 || fd == -1 || w != r)
+	{
+		free(buffer);
 		return (0);
+	}
 	free(buffer);
 	close(fd);
 	return (w);
