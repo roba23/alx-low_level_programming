ad202887c2f835d0a091972105cf8e166f485070
robel gebremariam <dekeborobel@gmail.com>
2023-02-07 23:14:12 +0000

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,7 +11,7 @@
 int main(int argc, char *argv[])
 {
 	int f1, f1r, f2, f2w, c1, c2;
-	char buffer[1024];
+	char buffer[65000];
 
 	if (argc != 3)
 	{
@@ -24,24 +24,25 @@ int main(int argc, char *argv[])
 	printf("f1r is = %d\n", f1r);
 	if (f1 < 0 || f1r < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]); 
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	c1 = close(f1);	
+	c1 = close(f1);
 	if (c1 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
 		exit(100);
 	}
 
-	f2 = open(argv[2], O_RDONLY | O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	f2 = open(argv[2], O_RDONLY | O_WRONLY | O_CREAT | O_TRUNC,
+		S_IWUSR | S_IRUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	f2w = write(f2, buffer, f1r);
 	if (f2 < 0 || f2w < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]); 
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	c2 = close(f2);	
+	c2 = close(f2);
 	if (c2 < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2);
