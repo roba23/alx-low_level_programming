37122c862d99dc12113dd638fea13d2c3a51c4b0
robel gebremariam <dekeborobel@gmail.com>
2023-02-07 19:42:26 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 	lseek(fd, 0, SEEK_SET);
-	if (strlen(text_content) > 0)
+	if (text_content != NULL)
 	{
 		i = write(fd, text_content, strlen(text_content));
 		if (i < 0)
