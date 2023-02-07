be3a5aac76bab68da41119bba8c71b439ababf14
robel gebremariam <dekeborobel@gmail.com>
2023-02-07 19:35:24 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,18 +4,20 @@
 #include <string.h>
 /**
  * create_file - create a file with read and write permission
- * for the user 
+ * for the user
  * @filename: the file name to create
  * @text_content: text to write to the created file
  * Return:1 on success -1 if it fails
  */
-int create_file(const char *filename,char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd, i;
-	
+
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_RDONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename,
+		O_CREAT | O_WRONLY | O_RDONLY | O_TRUNC,
+		S_IRUSR | S_IWUSR);
 	if (fd < 0)
 		return (-1);
 	if (strlen(text_content) > 0)
