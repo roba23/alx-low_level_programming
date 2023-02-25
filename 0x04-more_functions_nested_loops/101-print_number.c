01229d8ebc8f5148e84bc1fb4530d5eb3effd293
robel gebremariam <dekeborobel@gmail.com>
2023-02-25 14:27:00 +0000

    task 100

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 #include <limits.h>
+#include <math.h>
 /**
  * print_number - print number using putchat
  * @n: the number to print out
@@ -8,19 +9,25 @@
  */
 void print_number(int n)
 {
+	int flag = 0;
+
 	if (n == INT_MIN)
 	{
-		n = -2147483648;
+		_putchar('-');
+		n = (n + 1) * -1;
+		flag = 1;
 	}
-	if (n < 0)
+	if (n < 0 && n != INT_MIN)
 	{
 		_putchar('-');
-		n = n * -1;
+		n *= -1;
 	}
-	if (n / 10 != 0)
+	if (n / 10)
 	{
 		print_number(n / 10);
 	}
-	_putchar(n % 10 + '0');
+	if (flag)
+		_putchar(((n % 10) + 1) + '0');
+	else
+		_putchar((n % 10) + '0');
 }
-
