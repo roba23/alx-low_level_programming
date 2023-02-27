4f3e71c7df0a2b4da27566397fde511166f99ae2
robel gebremariam <dekeborobel@gmail.com>
2023-02-27 16:35:42 +0000

    made it little smaller

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,32 @@
-#import "main.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
 /**
- * main -
- * Return:
+ * main - finds key for a function
+ * Return: always zero
  */
-int main(void)
+int  main(void)
 {
+	int sum, i, j;
+	int count;
+
+	sum = count = j = 0;
+	for (i = 33; i <= 126; i++)
+	{
+
+		sum = 2772 % i;
+		if (sum == 0)
+		{
+			count = 2772 / i;
+			break;
+		}
+	}
+	for (j = 0; j < count; j++)
+	{
+		printf("%c", i);
+	}
+
+
 	return (0);
 }
