6bc60d7a9f64df6378dc09c82d232ec6eb5bccc4
robel gebremariam <dekeborobel@gmail.com>
2023-02-27 16:59:23 +0000

    adjusted the code to make it smaller

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,22 +10,26 @@ int  main(void)
 {
 	int sum, i, j;
 	int count;
+	char m[64];
 
 	sum = count = j = 0;
 	for (i = 33; i <= 126; i++)
 	{
 
 		sum = 2772 % i;
-		if (sum == 0)
+		count = 2772 / i;
+		
+		if (sum == 0 && count < 64 )
 		{
-			count = 2772 / i;
 			break;
 		}
 	}
 	for (j = 0; j < count; j++)
 	{
-		printf("%c", i);
+		m[j] = i;
 	}
+	m[j] = '\0';
+	printf("%s", m);
 
 
 	return (0);
