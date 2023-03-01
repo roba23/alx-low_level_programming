4683f211003d6f36a854469a778c4cdf115d8657
robel gebremariam <dekeborobel@gmail.com>
2023-03-01 20:08:09 +0000

    fixed for all characters to be included

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,23 +12,24 @@ char *rot13(char *a)
 	int len;
 	char output[1024];
 	char *p;
-	int i, j, m;
+	int i, j;
 
 	len = strlen(a);
-	i = j = m = 0;
+	i = j = 0;
 	for (i = 0; i < len; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (a[i] == alpha[j])
+			output[i] = a[i];
+			if (a[i] == alpha[j]) 
 			{
-				output[m] = key[j];
-				m++;
+				output[i] = key[j];
 				break;
 			}
+
 		}
 	}
-	output[m] = '\0';
+	output[i] = '\0';
 	p = output;
 	return (p);
 }
