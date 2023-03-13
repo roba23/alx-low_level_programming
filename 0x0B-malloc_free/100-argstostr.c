ac07ed36c8743f5dc0e31ced731a4e2f84bc46d5
robel gebremariam <dekeborobel@gmail.com>
2023-03-13 18:07:58 +0000

    task 100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -5,26 +5,27 @@
 /**
  * argstostr - convet arguments to strings
  * @ac: number of arguments
- * @av: pointers to strings of arrays
+ * @argv: pointers to strings of arrays
  * Return: pointer to string
  */
 char *argstostr(int ac, char **argv)
 {
 	int size, i, j, m;
 	char *hold;
+
 	i = j = size = m = 0;
 	if (ac == 0 || argv == NULL)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; argv[i][j]; j++)
-			{
-				size += 1;
+		{
+			size += 1;
 
-			}
+		}
 		size += 1;
 	}
-	hold = malloc((sizeof(char) * size) + 1) ;
+	hold = malloc((sizeof(char) * size) + 1);
 	for (i = 0; argv[i]; i++)
 	{
 		for (j = 0; argv[i][j]; j++)
