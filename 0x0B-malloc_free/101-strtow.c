950192963889edc490a1beeace5cf817ab5c25a0
robel gebremariam <dekeborobel@gmail.com>
2023-03-14 11:17:29 +0000

    task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -60,7 +60,7 @@ char **strtow(char *str)
 	p = malloc((sizeof(char **) * (column + 1)));
 	for (i = 0; i < column; i++)
 	{
-		p[i] = malloc(size_word[i] * sizeof(char));
+		p[i] = malloc(size_word[i] * sizeof(char) + 1);
 	}
 	j = column = 0;
 	for (i = 0; str[i]; i++)
