dc210f08859a3ce7f7a4379ac8d191efb450d0eb
robel gebremariam <dekeborobel@gmail.com>
2023-03-14 09:41:27 +0000

    task 101

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * strtow - convert string to words
+ * @str: string to be converted
+ * Return: pointer to the first array
+ */
+char **strtow(char *str)
+{
+	int oc , column, i, j;
+	char **p;/* pointer to pointersthat point to arrays */
+	int size_word[45];/*array to hold the size of each word */
+
+	oc = i = column = j = 0;
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] != ' ')
+		{
+			oc = 1;
+			j += 1;
+		}
+		if (oc && str[i] == ' ')
+		{
+			size_word[column] = j; 
+			oc = 0;
+			column += 1;
+			j = 0;
+		}
+	}
+	p = malloc((sizeof(char *) * column) + 1);
+	for (i = 0; i < column; i++)
+	{
+		p[i] = malloc((size_word[i] * sizeof(char)) + 1);
+	}
+	j = 0;
+	column = 0;
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] != ' ')
+		{
+			oc = 1;
+			p[column][j] = str[i];
+			j += 1;
+		}
+		if (oc && str[i] == ' ')
+		{
+			p[column][j] = '\0';
+			oc = 0;
+			column += 1;
+			j = 0;
+		}
+
+	}
+	if (oc)
+	{
+		p[column][j] = '\0';
+		p[column + 1] = NULL;
+	}
+	else
+		p[column] = NULL;
+	return (p);
+}
